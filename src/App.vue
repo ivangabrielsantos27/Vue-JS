47e89dbdafbb48b4eff0c33a5d8fdef84339e03a
ivangabriel.santos27 <ivangabriel.santos27@gmail.com>
2024-10-10 12:06:27 +0800

    Weather Application in Vue

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,179 @@
 <template>
-  <div>
-    <!-- <h2>App.vue</h2> -->
-    <TodoList/>
+  <div id="app" :class="typeof weather.main != 'undefined' && weather.main.temp > 24 ?
+  'warm' : ''">
+    <main>
+      <div class="search-box">
+        <input
+          type="text"
+          class="text search-bar"
+          placeholder="Search ..."
+          v-model="query"
+          @keypress="fetchWeather"
+        />
+      </div>
+
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
+        <div class="location-box">
+          <div class="location">
+            {{ weather.name }}, {{ weather.sys.country }}
+          </div>
+          <div class="date">{{ currentDate }}</div>
+        </div>
+
+        <div class="weather-box">
+          <div class="temp">{{ Math.round(weather.main.temp) }}°c</div>
+          <div class="weather">{{ weather.weather[0].main }}</div>
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
-import TodoList from './components/TodoList.vue'
-
 export default {
-  components: {
-    TodoList
-  }
+  name: "app",
+  data() {
+    return {
+      api_key: "60bea1be5e588ac2f41b13eb6021da05",
+      url_base: "https://api.openweathermap.org/data/2.5/",
+      query: "",
+      weather: {},
+    };
+  },
+  computed: {
+    currentDate() {
+      const now = new Date();
+      return now.toLocaleDateString("en-US", {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      });
+    },
+  },
+  methods: {
+    fetchWeather(e) {
+      if (e.key == "Enter") {
+        fetch(
+          `${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`
+        )
+          .then((res) => {
+            if (!res.ok) {
+              throw new Error("City not found");
+            }
+            return res.json();
+          })
+          .then(this.setResults)
+          .catch((error) => {
+            console.error("Error fetching weather data:", error);
+            alert(error.message);
+          });
+      }
+    },
+    setResults(results) {
+      this.weather = results;
+    },
+  },
 };
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
 body {
-  background-color: rgb(66, 66, 66);
-  color: aliceblue;
+  font-family: "montserrat", sans-serif;
+}
+
+#app {
+  background-image: url("https://manybackgrounds.com/images/hd/winter-weather-by-the-lake-xuzvfz6y3k0ho01h.jpg");
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4s;
+}
+
+#app.warm {
+  background-image: url('https://cdn.pixabay.com/photo/2023/11/20/18/21/sunset-8401670_1280.jpg');
+}
+
+main {
+  min-height: 100vh;
+  padding: 25px;
+  background-image: linear-gradient(
+    to bottom,
+    rgba(0, 0, 0, 0.25),
+    rgba(0, 0, 0, 0.75)
+  );
+}
+
+.search-box {
+  width: 100%;
+  margin-bottom: 30px;
+}
+
+.search-box .search-bar {
+  display: block;
+  width: 100%;
+  padding: 15px;
+  color: #313131;
+  font-size: 20px;
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 0 16px 0 16px;
+  transition: 0.4s;
+  box-shadow: 0 0 8px rgba(0, 0, 0, 0.25);
+}
+
+.search-box .search-bar:focus {
+  background-color: rgba(255, 255, 255, 0.75);
+  box-shadow: 0 0 16px rgba(0, 0, 0, 0.25);
+  border-radius: 16px 0 16px 0;
+}
+
+.location-box .location {
+  color: white;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+}
+
+.location-box .date {
+  color: white;
+  font-size: 32px;
+  font-weight: 300;
+  font-style: italic;
+  text-align: center;
+}
+
+.weather-box {
+  text-align: center;
+}
+
+.weather-box .temp {
+  display: inline-block;
+  padding: 10px 25px;
+  color: white;
+  font-size: 102px;
+  font-weight: 900;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.25);
+  border-radius: 16px;
+  margin: 30px 0;
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+
+.weather-box .weather {
+  color: white;
+  font-size: 48px;
+  font-weight: 700;
+  font-style: italic;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
 }
 </style>
