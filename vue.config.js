1eba5f678a606901483b7051c53e10a3a6e90719
ivangabriel.santos27 <ivangabriel.santos27@gmail.com>
2024-10-11 18:55:20 +0800

    Single File Component Todo List App in Vue

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,11 @@
+// FOR DEBUGGIN - ENABLES THE APP TO RUN EVEN IF THERE ARE OBJS/VARS NOT USED
+
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+//   lintOnSave: false  // Add this line to disable ESLint on save
+// })
+
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
